8ff7d41f630c6d16c017a5ecfaf8f7a117845bf6
sagar sen <sagarsen088@gmail.com>
2020-07-24 18:01:56 +0530

    first commit

---
+++ src/HtmlLayoutEx.java
@@ -0,0 +1,15 @@
+import org.apache.log4j.Logger;  
+   
+public class HtmlLayoutEx  
+{  
+    static Logger log = Logger.getLogger(HtmlLayoutEx.class);  
+   
+    public static void main(String[] args)  
+    {  
+   
+        log.debug("Sample debug message");  
+        log.info("Sample info message");  
+        log.error("Sample error message");  
+        log.fatal("Sample fatal message");  
+    }  
+}  
